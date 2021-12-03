2bc8e7d7d5238cb16ca4b5bb0f94f1daf98764d4
mrtlckn <ahmet.telceken@hotmail.com>
2021-12-03 12:56:22 +0300

    Classes and Uml Desing
    Create classes for OOP project which has Engine and fuel tank

---
+++ OOP 2021/Step 3/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include "Engine.h"
+#include "Tank.h"
+#include "Read-Writing.h"
+
+using namespace std;
+
+int main() {
+	PrintClass yazdir;
+	yazdir.PrintMethod();
+
+	return 0;
+}
